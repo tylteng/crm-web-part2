5ec5623f31d9902bd880e063fa256aa940f5b6bf
tylteng <teng.ty4@gmail.com>
2017-11-20 21:22:48 -0500

    completed exercise

--- crm.rb
+++ crm.rb
@@ -14,6 +14,15 @@ get '/about' do
   erb :about
 end
 
+get '/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :single_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 after do
   ActiveRecord::Base.connection.close
 end
