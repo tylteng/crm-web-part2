ef0c4ad9b9d6c35fb4b28d9f29c49f53544d791e
tylteng <teng.ty4@gmail.com>
2017-11-26 18:29:40 -0500

    work in progress

--- crm.rb
+++ crm.rb
@@ -1,20 +1,30 @@
 require 'sinatra'
 require_relative 'contact'
 
-get '/' do
+get '/contacts' do
   @all_contacts = Contact.all
   erb :index
 end
 
-get '/contacts' do
-  erb :contacts
+get '/contacts/new' do
+  erb :new
+end
+
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/')
 end
 
 get '/about' do
   erb :about
 end
 
-get '/:id' do
+get '/contacts/:id' do
   @contact = Contact.find_by(id: params[:id].to_i)
   if @contact
     erb :single_contact
